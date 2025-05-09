dfe46fffe0f1a615738c9ed18cee8879855637fd
Frank <frank99-xu@outlook.com>
2025-05-10 04:27:52 +0800

    feat: update to Mantine v8

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,20 +15,12 @@ const config: Config = {
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'iZJU', // Usually your GitHub org/user name.
-  projectName: 'iZJU-Wiki', // Usually your repo name.
-
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'zh-Hans',
-    locales: ['zh-Hans', 'en'],
+    locales: ['zh-Hans'],
   },
 
   presets: [
@@ -37,8 +29,6 @@ const config: Config = {
       {
         docs: {
           sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/frank-xjh/iZJU-Wiki/tree/main/',
         },
@@ -64,8 +54,6 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
     navbar: {
       title: 'iZJU Wiki',
       logo: {
